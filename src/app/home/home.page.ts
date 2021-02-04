522fd43b84e7e9c3f149b2b1a143a9bd6ec6193b
Makwale <makwale.em@gmail.com>
2021-02-04 03:15:08 -0800

    Fist commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner/ngx';
+
+
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +11,35 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private qrScanner: QRScanner) {}
+
+  scan(){
+    this.qrScanner.prepare()
+    .then((status: QRScannerStatus) => {
+      if (status.authorized) {
+        // camera permission was granted
+
+
+        // start scanning
+        let scanSub = this.qrScanner.scan().subscribe((text: string) => {
+          alert('Scanned something' + text);
+
+         // this.qrScanner.hide(); // hide camera preview
+          //scanSub.unsubscribe(); // stop scanning
+        });
+
+      } else if (status.denied) {
+        alert("Permision dinied permanently")
+        // camera permission was permanently denied
+        // you must use QRScanner.openSettings() method to guide the user to the settings page
+        // then they can grant the permission from there
+      } else {
+        alert("Permision dinied temporaly")
+        // permission was denied, but not permanently. You can ask for permission again at a later time.
+      }
+    })
+    .catch((e: any) => alert('Error is' + e));
+
+  }
 
 }
